8f7ea9f6061ba17a8e5483e8f0582fdf627ccbbf
flydev <code@sekretservices.com>
2023-04-05 23:11:22 +0200

    added: Utils functions with windows support
    - removed opencoff/go-utils

---
+++ utils/askpass.go
@@ -0,0 +1,56 @@
+// askpass.go -- Interactive password prompt
+//
+// (c) 2016 Sudhi Herle <sudhi@herle.net>
+//
+// # Placed in the Public Domain
+//
+// This software does not come with any express or implied
+// warranty; it is provided "as is". No claim  is made to its
+// suitability for any purpose.
+package utils
+
+import (
+	"fmt"
+	"syscall"
+
+	// updated to use golang.org/x/term
+	"golang.org/x/term"
+)
+
+// Askpass prompts user for an interactive password.
+// If verify is true, confirm a second time.
+// Mistakes during confirmation cause the process to restart upto a
+// maximum of 2 times.
+func Askpass(prompt string, verify bool) (string, error) {
+
+	for i := 0; i < 2; i++ {
+		fmt.Printf("%s: ", prompt)
+		pw1, err := term.ReadPassword(int(syscall.Stdin))
+		fmt.Printf("\n")
+		if err != nil {
+			return "", err
+		}
+		if !verify {
+			return string(pw1), nil
+		}
+
+		fmt.Printf("%s again: ", prompt)
+		pw2, err := term.ReadPassword(int(syscall.Stdin))
+		fmt.Printf("\n")
+		if err != nil {
+			return "", err
+		}
+
+		a := string(pw1)
+		b := string(pw2)
+		if a == b {
+			return a, nil
+		}
+
+		fmt.Printf("** password mismatch; try again ..\n")
+	}
+
+	return "", fmt.Errorf("too many tries getting password")
+}
+
+// vim: ft=go:sw=8:ts=8:noexpandtab:tw=98:
